bbd901159888f91180f3ce6ea9da4d6a2966182a
SergeySidorchuk <serg_ss@ukr.net>
2021-07-22 23:43:42 +0300

    added ContactsForm, List, Filter

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
+import shortid from 'shortid';
 import PropTypes from 'prop-types';
+import './App.css';
 
+import ContactsList from "./components/ContactsList";
+import ContactsForm from "./components/ContactsForm/ContactsForm";
+import Filter from "./components/Filter";
+
+
+class App extends Component {
+    
+}
